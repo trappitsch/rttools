791dc8b1cdb4ea6c46d3e6710ef843e2407cfd14
Reto Trappitsch <reto@galactic-forensics.space>
2022-04-25 19:05:45 -0400

    Added regression according to Thomas' new paper
    - Including tests with Mahon's data and with Thomas' table

--- rttools/linreg.py
+++ rttools/linreg.py
@@ -1,6 +1,9 @@
 """Linear fits with uncertainties in both axes according to Mahon et al. (1996)"""
+from typing import Tuple
+import warnings
 
 import numpy as np
+from numpy.polynomial import Polynomial
 import scipy.stats as stats
 
 from .utils import kron_delta
@@ -220,16 +223,16 @@ class Mahon:
                 vi = yi - ybar
                 # add to sums
                 btop += (
-                    wi ** 2.0
+                    wi**2.0
                     * vi
-                    * (ui * syi ** 2.0 + bcalc * vi * sxi ** 2.0 - pit * vi * sxi * syi)
+                    * (ui * syi**2.0 + bcalc * vi * sxi**2.0 - pit * vi * sxi * syi)
                 )
                 bbot += (
-                    wi ** 2.0
+                    wi**2.0
                     * ui
                     * (
-                        ui * syi ** 2.0
-                        + bcalc * vi * sxi ** 2.0
+                        ui * syi**2.0
+                        + bcalc * vi * sxi**2.0
                         - bcalc * pit * ui * sxi * syi
                     )
                 )
@@ -302,17 +305,17 @@ class Mahon:
             ui = xi - xbar
             vi = yi - ybar
             sxyi = pit * sxi * syi
-            sum1 += wi ** 2.0 * (
-                2 * b * (ui * vi * sxi ** 2.0 - ui ** 2.0 * sxyi)
-                + (ui ** 2.0 * syi ** 2.0 - vi ** 2 * sxi ** 2.0)
+            sum1 += wi**2.0 * (
+                2 * b * (ui * vi * sxi**2.0 - ui**2.0 * sxyi)
+                + (ui**2.0 * syi**2.0 - vi**2 * sxi**2.0)
             )
             sum2 += (
-                wi ** 3.0
-                * (sxyi - b * sxi ** 2.0)
+                wi**3.0
+                * (sxyi - b * sxi**2.0)
                 * (
-                    b ** 2.0 * (ui * vi * sxi ** 2 - ui ** 2 * sxyi)
-                    + b * (ui ** 2 * syi ** 2 - vi ** 2 * sxi ** 2)
-                    - (ui * vi * syi ** 2 - vi ** 2 * sxyi)
+                    b**2.0 * (ui * vi * sxi**2 - ui**2 * sxyi)
+                    + b * (ui**2 * syi**2 - vi**2 * sxi**2)
+                    - (ui * vi * syi**2 - vi**2 * sxyi)
                 )
             )
         dthdb = sum1 + 4.0 * sum2
@@ -347,23 +350,23 @@ class Mahon:
                 sxyj = pjt * sxj * syj
                 # add to dthdxi and dthdyi
                 dthdxi += (
-                    wj ** 2.0
+                    wj**2.0
                     * (kron_delta(it, jt) - wi / wksum)
                     * (
-                        b ** 2 * (vj * sxj ** 2 - 2 * uj * sxyj)
-                        + 2 * b * uj * syj ** 2
-                        - vj * syj ** 2
+                        b**2 * (vj * sxj**2 - 2 * uj * sxyj)
+                        + 2 * b * uj * syj**2
+                        - vj * syj**2
                     )
                 )
                 # correct equation! not equal to equation 21 in Mahon (1996)
                 dthdyi += (
-                    wj ** 2.0
+                    wj**2.0
                     * (kron_delta(it, jt) - wi / wksum)
                     * (
-                        b ** 2 * uj * sxj ** 2
+                        b**2 * uj * sxj**2
                         + 2 * vj * sxyj
-                        - 2 * b * vj * sxj ** 2.0
-                        - uj * syj ** 2
+                        - 2 * b * vj * sxj**2.0
+                        - uj * syj**2
                     )
                 )
 
@@ -373,18 +376,18 @@ class Mahon:
 
             # now finally add to sigasq and sigbsq
             sigbsq += (
-                dthdxi ** 2.0 * sxi ** 2.0
-                + dthdyi ** 2.0 * syi ** 2.0
+                dthdxi**2.0 * sxi**2.0
+                + dthdyi**2.0 * syi**2.0
                 + 2 * sxyi * dthdxi * dthdyi
             )
             sigasq += (
-                dadxi ** 2.0 * sxi ** 2.0
-                + dadyi ** 2.0 * syi ** 2.0
+                dadxi**2.0 * sxi**2.0
+                + dadyi**2.0 * syi**2.0
                 + 2 * sxyi * dadxi * dadyi
             )
 
         # now divide sigbsq
-        sigbsq /= dthdb ** 2.0
+        sigbsq /= dthdb**2.0
 
         # now write slope uncertainty and y intercept uncertainty back to class
         if calcxintunc:
@@ -410,4 +413,360 @@ class Mahon:
         self.mswd = mswd
 
     def calc_wi(self, sx, sy, b, p):
-        return 1.0 / (sy ** 2 + b ** 2 * sx ** 2 - 2 * b * p * sx * sy)
+        return 1.0 / (sy**2 + b**2 * sx**2 - 2 * b * p * sx * sy)
+
+
+class Stephan:
+    """Stephan and Trappitsch (2022) linear regression.
+
+    Follows the linear regression lined out in Stephan and Trappitsch (2022).
+    Todo: add DOI of the paper, once published.
+    Todo: add Example of usage
+    """
+
+    def __init__(
+        self,
+        xdat: np.ndarray,
+        ydat: np.ndarray,
+        sigx: np.ndarray,
+        sigy: np.ndarray,
+        rho: np.ndarray = None,
+        fixpt: np.ndarray = None,
+        autocalc=True,
+        **kwargs,
+    ):
+        """Initialize the class.
+
+        :param xdat: X data.
+        :param ydat: Y data.
+        :param sigx: 1 sigma uncertainty of x data.
+        :param sigy: 1 sigma uncertainty of y data.
+        :param rho: Correlation between x and y data, defaults to no correlation.
+        :param fixpt: Fixed point through which regression needs to go.
+        :param autocalc: Automatically calculate the regression and print params.
+        :param kwargs: Additional keyword arguments:
+            "iter_max": Maximum iteration limit for slope (default 1e6)
+            "reg_limit": Regression limit for slope (default: 1e-6)
+
+        raises ValueError: Fix point is of the wrong shape.
+        """
+        self.xdat = np.array(xdat)
+        self.ydat = np.array(ydat)
+        self.sigx = np.array(sigx)
+        self.sigy = np.array(sigy)
+        if rho is None:
+            self.rho = np.zeros_like(self.xdat)
+        else:
+            self.rho = np.array(rho)
+
+        # calculate correlated uncertainty sigxy
+        self.sigxy = self.rho * self.sigx * self.sigy
+
+        if fixpt is not None:
+            fixpt = np.array(fixpt)
+            if fixpt.shape != (2,):
+                raise ValueError("Fix point must be of the form [x_fix, yfix].")
+        self.fix_pt = fixpt
+
+        # Initialize the slope and intercept
+        self._slope = None
+        self._slope_unc = None
+        self._intercept = None
+        self._intercept_unc = None
+        self._chi_squared = None
+        self._mswd = None
+
+        # keyword arguments
+        if "regression_limit" in kwargs:
+            self.reg_limit = kwargs["regression_limit"]
+        else:
+            self.reg_limit = 1e-6
+        if "iter_max" in kwargs:
+            self.iter_max = kwargs["iter_max"]
+        else:
+            self.iter_max = 1e6
+
+        # helper variables
+        self.xbar = None
+        self.ybar = None
+        self.weights = None
+
+        if autocalc:
+            self.calculate()
+
+    @property
+    def chi_squared(self) -> float:
+        """Return chi_squared of the regression."""
+        return self._chi_squared
+
+    @property
+    def intercept(self) -> Tuple[float, float]:
+        """Return intercept and its 1 sigma uncertainty."""
+        return self._intercept, self._intercept_unc
+
+    @property
+    def mswd(self) -> float:
+        """Return MSWD of the regression."""
+        return self._mswd
+
+    @property
+    def parameters(self) -> np.ndarray:
+        """Return all parameters of the linear regression.
+
+        :return: slope, intercept, slope_uncertainty, intercept_uncertainty, MSWD
+        """
+        return np.array(
+            [
+                self._slope,
+                self._intercept,
+                self._slope_unc,
+                self._intercept_unc,
+                self._mswd,
+            ]
+        )
+
+    @property
+    def slope(self) -> Tuple[float, float]:
+        """Return slope and its 1 sigma uncertainty."""
+        return self._slope, self._slope_unc
+
+    def calculate(self):
+        """Do the linear regression and save the parameters in the class variables."""
+        self.slope_initial_guess()
+        self.slope_calculation()
+        self._intercept = self.ybar - self._slope * self.xbar
+        self.unc_calculation()
+        self.goodness_of_fit()
+
+    def goodness_of_fit(self):
+        """Calculate goodness of fit parameters chi-squared and MSWD."""
+        chi_sq = np.sum(
+            self.weights * (self.ydat - self._slope * self.xdat - self._intercept) ** 2
+        )
+        self._chi_squared = chi_sq
+
+        dof = len(self.xdat) - 2 if self.fix_pt is None else len(self.xdat) - 1
+        self._mswd = chi_sq / dof
+
+    def slope_calculation(self):
+        """Iterate the slope until it fits."""
+
+        def calc_weights(b: float):
+            """Calculate weights and return them."""
+            return 1 / (self.sigy**2 + b**2 * self.sigx**2 - 2 * b * self.sigxy)
+
+        def calc_xbar(weights: np.ndarray):
+            """Calculate x bar and return it.
+
+            :param weights: Weights.
+            """
+            if self.fix_pt is None:
+                return np.sum(weights * self.xdat) / np.sum(weights)
+            else:
+                return self.fix_pt[0]
+
+        def calc_ybar(weights: np.ndarray):
+            """Calculate y bar and return it.
+
+            :param weights: Weights.
+            """
+            if self.fix_pt is None:
+                return np.sum(weights * self.ydat) / np.sum(weights)
+            else:
+                return self.fix_pt[1]
+
+        def iterate_b(b_old):
+            """Do one iteration step with the slope and return the new value."""
+            b = b_old
+            weights = calc_weights(b)
+            u_all = self.xdat - calc_xbar(weights)
+            v_all = self.ydat - calc_ybar(weights)
+            b_new = np.sum(
+                weights**2
+                * v_all
+                * (
+                    u_all * self.sigy**2
+                    + b * v_all * self.sigx**2
+                    - v_all * self.sigxy
+                )
+            ) / np.sum(
+                weights**2
+                * u_all
+                * (
+                    u_all * self.sigy**2
+                    + b * v_all * self.sigx**2
+                    - b * u_all * self.sigxy
+                )
+            )
+            return b_new
+
+        # iterate until solution is found
+        iter_cnt = 0
+        b_old = self._slope
+        b_new = iterate_b(b_old)
+        while np.abs(b_old - b_new) > self.reg_limit and iter_cnt <= self.iter_max:
+            b_old = b_new
+            b_new = iterate_b(b_old)
+            iter_cnt += 1
+
+        if iter_cnt == self.iter_max:
+            warnings.warn(
+                f"Iteration count for slope optimization hit the limt at "
+                f"{self.iter_max}. The current difference between the old and new "
+                f"slope is {np.abs(b_old - b_new)}"
+            )
+
+        self._slope = b_new
+        self.weights = calc_weights(b_new)
+        self.xbar = calc_xbar(self.weights)
+        self.ybar = calc_ybar(self.weights)
+
+    def slope_initial_guess(self):
+        """Calculate an initial guess of the slope without uncertainties and save it."""
+        polyfit = Polynomial.fit(self.xdat, self.ydat, deg=1)
+        self._slope = polyfit.convert().coef[1]
+
+    def unc_calculation(self):
+        """Calculate uncertainties for slope and intercept with no fixed point."""
+        # helper variables
+        sigx = self.sigx
+        sigy = self.sigy
+        sigxy = self.sigxy
+        b = self._slope
+        weights = self.weights
+        xbar = self.xbar
+        ybar = self.ybar
+        u_all = self.xdat - xbar
+        v_all = self.ydat - ybar
+
+        sum_weights = np.sum(weights)
+
+        # d(\theta) / db
+        dthdb = np.sum(
+            weights**2
+            * (
+                2 * b * (u_all * v_all * sigx**2 - u_all**2 * sigxy)
+                + (u_all**2 * sigy**2 - v_all**2 * sigx**2)
+            )
+        ) + 4 * np.sum(
+            weights**3
+            * (sigxy - b * sigx**2)
+            * (
+                b**2 * (u_all * v_all * sigx**2 - u_all**2 * sigxy)
+                + b * (u_all**2 * sigy**2 - v_all**2 * sigx**2)
+                - (u_all * v_all * sigy**2 - v_all**2 * sigxy)
+            )
+        )
+
+        # d(\theta) / dxi
+        def calc_dtheta_dxi(it: int):
+            """Calculate partial derivative d(\theta)/dxi.
+
+            :param ind: Index where the $i$ is at.
+            """
+            if self.fix_pt is None:
+                sum_all = 0.0
+                for jt, wj in enumerate(weights):
+                    kron = kron_delta(it, jt)
+                    sum_all += (
+                        wj**2
+                        * (kron - weights[it] / sum_weights)
+                        * (
+                            b**2 * v_all[jt] * sigx[jt] ** 2
+                            - b**2 * 2 * u_all[jt] * sigxy[jt]
+                            + 2 * b * u_all[jt] * sigy[jt] ** 2
+                            - v_all[jt] * sigy[jt] ** 2
+                        )
+                    )
+                return sum_all
+            else:
+                return weights[it] ** 2 * (
+                    b**2 * v_all[it] * sigx[it] ** 2
+                    - b**2 * 2 * u_all[it] * sigxy[it]
+                    + 2 * b * u_all[it] * sigy[it] ** 2
+                    - v_all[it] * sigy[it] ** 2
+                )
+
+        # d(\theta) / dyi
+        def calc_dtheta_dyi(it: int):
+            """Calculate partial derivative d(\theta)/dyi.
+
+            :param ind: Index where the $i$ is at.
+            """
+            if self.fix_pt is None:
+                sum_all = 0.0
+                for jt, wj in enumerate(weights):
+                    kron = kron_delta(it, jt)
+                    sum_all += (
+                        wj**2
+                        * (kron - weights[it] / sum_weights)
+                        * (
+                            b**2 * u_all[jt] * sigx[jt] ** 2
+                            - 2 * b * v_all[jt] * sigx[jt] ** 2
+                            - u_all[jt] * sigy[jt] ** 2
+                            + 2 * v_all[jt] * sigxy[jt]
+                        )
+                    )
+                return sum_all
+            else:
+                return weights[it] ** 2 * (
+                    b**2 * u_all[it] * sigx[it] ** 2
+                    - 2 * b * v_all[it] * sigx[it] ** 2
+                    - u_all[it] * sigy[it] ** 2
+                    + 2 * v_all[it] * sigxy[it]
+                )
+
+        # da / dxi
+        def calc_da_dxi(it: int):
+            """Calculate partial derivative da/dxi.
+
+            :param ind: Index where the $i$ is at.
+            """
+            if self.fix_pt is None:
+                return (
+                    -b * weights[it] / sum_weights - xbar * calc_dtheta_dxi(it) / dthdb
+                )
+            else:
+                return -xbar * calc_dtheta_dxi(it) / dthdb
+
+        # da / dyi
+        def calc_da_dyi(it: int):
+            """Calculate partial derivative da/dyi.
+
+            :param ind: Index where the $i$ is at.
+            """
+            if self.fix_pt is None:
+                return weights[it] / sum_weights - xbar * calc_dtheta_dyi(it) / dthdb
+            else:
+                return -xbar * calc_dtheta_dyi(it) / dthdb
+
+        # calculate uncertainty for slope
+        sigb_sq = 0.0
+        for it, sigxi in enumerate(sigx):
+            sigyi = sigy[it]
+            sigxyi = sigxy[it]
+            dtheta_dxi = calc_dtheta_dxi(it)
+            dtheta_dyi = calc_dtheta_dyi(it)
+            sigb_sq += (
+                dtheta_dxi**2 * sigxi**2
+                + dtheta_dyi**2 * sigyi**2
+                + 2 * sigxyi * dtheta_dxi * dtheta_dyi
+            )
+        sigb_sq /= dthdb**2
+        self._slope_unc = np.sqrt(sigb_sq)
+
+        if self.fix_pt is None:
+            siga_sq = 0.0
+            for it, sigxi in enumerate(sigx):
+                sigyi = sigy[it]
+                sigxyi = sigxy[it]
+                da_dxi = calc_da_dxi(it)
+                da_dyi = calc_da_dyi(it)
+                siga_sq += (
+                    da_dxi**2 * sigxi**2
+                    + da_dyi**2 * sigyi**2
+                    + 2 * sigxyi * da_dxi * da_dyi
+                )
+        else:
+            siga_sq = self.fix_pt[0] ** 2 * sigb_sq
+        self._intercept_unc = np.sqrt(siga_sq)
--- tests/func/test_linreg.py
+++ tests/func/test_linreg.py
@@ -1,8 +1,9 @@
 """Function tests for linear fitting routine - mahon.py."""
 
+import pytest
 import numpy as np
 
-from rttools.linreg import Mahon
+from rttools.linreg import Mahon, Stephan
 
 # MAHON TEST DATA FROM PAPER #
 MAHON_XDAT = np.array([0.037, 0.035, 0.032, 0.040, 0.013, 0.038, 0.042, 0.030])
@@ -22,6 +23,12 @@ MAHON_RES_CORR = np.array(
     [-0.07752, 0.01027, 0.003684, 0.000375, 0.04753, 0.00169, 1.04]
 )
 
+# results from Stephan & Trappitsch (2022) for fixed intercept Mahon Data
+STEPHAN_FIX_PT = np.array([0.01, 0.003])
+STEPHAN_RES_FIXED = np.array(
+    [-0.080842, 0.002222, 0.0038084, 0.0000222, np.nan, np.nan, 0.911]
+)
+
 
 def test_mahon_calculate_with_ci_setup():
     """Assure setup and some sanity checks."""
@@ -35,3 +42,58 @@ def test_mahon_calculate_with_ci_setup():
 
     ci_xax_expected = np.linspace(np.min(MAHON_XDAT), np.max(MAHON_XDAT), bins)
     np.testing.assert_equal(reg.ci_xax, ci_xax_expected)
+
+
+def test_stephan_fit_correlated():
+    """Test Stephan fit with correlated Mahon data."""
+    reg = Stephan(MAHON_XDAT, MAHON_YDAT, MAHON_XDAT_UNC, MAHON_YDAT_UNC, rho=MAHON_RHO)
+    slope_exp = MAHON_RES_CORR[0:2]
+    intercept_exp = MAHON_RES_CORR[2:4]
+    mswd_exp = MAHON_RES_CORR[6]
+
+    slope_rec = np.array(reg.slope)
+    intercept_rec = np.array(reg.intercept)
+    mswd_rec = reg.mswd
+
+    assert slope_exp == pytest.approx(slope_rec, abs=1e-5)
+    assert intercept_exp == pytest.approx(intercept_rec, abs=1e-5)
+    assert mswd_exp == pytest.approx(mswd_rec, abs=0.01)
+
+
+def test_stephan_fit_uncorrelated():
+    """Test Stephan fit with uncorrelated Mahon data."""
+    reg = Stephan(MAHON_XDAT, MAHON_YDAT, MAHON_XDAT_UNC, MAHON_YDAT_UNC)
+    slope_exp = MAHON_RES_UNCORR[0:2]
+    intercept_exp = MAHON_RES_UNCORR[2:4]
+    mswd_exp = MAHON_RES_UNCORR[6]
+
+    slope_rec = np.array(reg.slope)
+    intercept_rec = np.array(reg.intercept)
+    mswd_rec = reg.mswd
+
+    assert slope_exp == pytest.approx(slope_rec, abs=1e-5)
+    assert intercept_exp == pytest.approx(intercept_rec, abs=1e-5)
+    assert mswd_exp == pytest.approx(mswd_rec, abs=0.01)
+
+
+def test_stephan_fit_correlated_fixed():
+    """Test Stephan regression with Mahon Data and Fixed intercept."""
+    reg = Stephan(
+        MAHON_XDAT,
+        MAHON_YDAT,
+        MAHON_XDAT_UNC,
+        MAHON_YDAT_UNC,
+        rho=MAHON_RHO,
+        fixpt=STEPHAN_FIX_PT,
+    )
+    slope_exp = STEPHAN_RES_FIXED[0:2]
+    intercept_exp = STEPHAN_RES_FIXED[2:4]
+    mswd_exp = STEPHAN_RES_FIXED[6]
+
+    slope_rec = np.array(reg.slope)
+    intercept_rec = np.array(reg.intercept)
+    mswd_rec = reg.mswd
+
+    assert slope_exp == pytest.approx(slope_rec, abs=1e-6)
+    assert intercept_exp == pytest.approx(intercept_rec, abs=1e-6)
+    assert mswd_exp == pytest.approx(mswd_rec, abs=0.01)
