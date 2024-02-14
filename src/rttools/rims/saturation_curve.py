3c226ae5fae2b5739ad945da74856a9d156e16cb
Reto Trappitsch <reto@galactic-forensics.space>
2024-02-14 14:38:36 +0100

    Add functionality to plot saturation curves

--- src/rttools/rims/saturation_curve.py
+++ src/rttools/rims/saturation_curve.py
@@ -1,1 +1,155 @@
 """Plot some good-looking saturation curves from measurements."""
+
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+from rttools import latex
+from rttools import ureg
+
+
+def saturation_curve(
+    xdata: np.ndarray,
+    ydata: np.ndarray,
+    xunit: Union[ureg.Quantity, str] = None,
+    yunit: Union[ureg.Quantity, str] = None,
+    xlabel: str = None,
+    ylabel: str = "Signal",
+    fit=True,
+) -> plt.Figure:
+    """Plot a saturation curve from data.
+
+    If fitting is desired, a Letokhov saturation curve is fitted to the data and plotted.
+    The fit parameters are printed on the plot.
+    The fit is done with the `scipy.optimize.curve_fit` function and only considers
+    uncertainties in the y-axis data, if given.
+    The fit function is `n = ni + nmax * (1 - exp(-x / isat))`.
+
+    :param xdata: x-axis data. Can contain two columns for error bars.
+    :param ydata: y-axis data. Can contain two columns for error bars.
+    :param xunit: Unit for x-axis data. If `xdata` is unitful, this is ignored.
+    :param yunit: Unit for y-axis data. If `ydata` is unitful, this is ignored.
+    :param xlabel: Label for x-axis. If None, try to infer from xunit.
+        Infers "Power" if xunit is in W or "Irradiance" if xunit is in W/cm^2.
+    :param fit: If True, fit a Letokhov saturation curve to the data, plot it,
+        and mark up with fit parameters.
+
+    :return: Matplotlib axis
+    """
+    if isinstance(xdata, ureg.Quantity):
+        xunit = xdata.units
+        xdata = xdata.magnitude
+    elif isinstance(xunit, str):
+        xunit = ureg.Unit(xunit)
+
+    if isinstance(ydata, ureg.Quantity):
+        yunit = ydata.units
+        ydata = ydata.magnitude
+    elif isinstance(yunit, str):
+        yunit = ureg.Unit(yunit)
+
+    # latex units if not none
+    xunit_ltx = None
+    yunit_ltx = None
+    if xunit is not None:
+        xunit_ltx = f"{xunit:P~}"
+    if yunit is not None:
+        yunit_ltx = f"{yunit:P~}"
+
+    # infer xlabel if not given
+    if xunit is not None:
+        if xlabel is None:
+            if xunit.compatible_units() == ureg.W.compatible_units():
+                xlabel = "Power"
+            elif xunit.compatible_units() == (ureg.W / ureg.cm**2).compatible_units():
+                xlabel = "Irradiance"
+
+    # split xdata and ydata into xdata, xerr and ydata, yerr
+    if xdata.ndim == 2:
+        xerr = xdata[1]
+        xdata = xdata[0]
+    else:
+        xerr = None
+    if ydata.ndim == 2:
+        yerr = ydata[1]
+        ydata = ydata[0]
+    else:
+        yerr = None
+
+    # create the figure
+    fig, ax = plt.subplots(1, 1)
+
+    # plot the data
+    ax.errorbar(
+        xdata,
+        ydata,
+        xerr=xerr,
+        yerr=yerr,
+        marker="o",
+        label="Data",
+        linestyle="None",
+        linewidth=0.5,
+        color="tab:blue",
+    )
+
+    # Axes labels
+    if xlabel:
+        if xunit_ltx:
+            ax.set_xlabel(f"{xlabel} ({xunit_ltx})")
+        else:
+            ax.set_xlabel(f"{xlabel}")
+
+    if ylabel:
+        if yunit_ltx is not None:
+            ax.set_ylabel(f"{ylabel} ({yunit_ltx})")
+        else:
+            ax.set_ylabel(f"{ylabel} (arb)")
+
+    # fit a curve if desired
+    if fit:
+        # take an initial guess from the data
+        initial_guess = [0, 1, np.max(xdata) / 2]
+
+        popt, pcov = curve_fit(
+            _letokhov,
+            xdata,
+            ydata,
+            p0=initial_guess,
+            sigma=yerr,
+        )
+        ni, nmax, isat = popt
+        xfit = np.linspace(xdata.min(), xdata.max(), 1000)
+        yfit = _letokhov(xfit, ni, nmax, isat)
+
+        ax.plot(xfit, yfit, label="Fit", color="darkred", linestyle="-")
+        ax.legend(loc="upper left")
+
+        fit_string = "Fit parameters:\n"
+        fit_string += (
+            f"I$_\\mathrm{{sat}}$ = {latex.exp_notation(isat, 2)} {xunit_ltx}\n"
+        )
+        fit_string += f"N$_\\mathrm{{max}}$ = {nmax:.2f}"
+
+        ax.text(
+            0.96,
+            0.05,
+            fit_string,
+            color="darkred",
+            transform=ax.transAxes,
+            va="bottom",
+            ha="right",
+            ma="left",
+            bbox=dict(
+                boxstyle="round", facecolor="white", alpha=0.7, edgecolor="darkred"
+            ),
+        )
+
+    fig.tight_layout()
+    return fig
+
+
+def _letokhov(x, ni, nmax, isat):
+    """Letokhov saturation curve."""
+    return ni + nmax * (1 - np.exp(-x / isat))
--- tests/func/rims/test_saturation_curve.py
+++ tests/func/rims/test_saturation_curve.py
@@ -1,3 +1,42 @@
-# Tests for saturation curve module
+# Tests for saturation curve module - these are simply run-through tests!:w
 
 
+import pytest
+import numpy as np
+
+import rttools.rims.saturation_curve as sc
+from rttools import ureg
+
+XDAT = np.array([27821, 22257, 13911, 6955, 2782, 31994, 25039, 18084, 0])
+YDAT = np.array([1.0, 0.8535, 0.5161, 0.4816, 0.3486, 1.0358, 0.9622, 0.7882, 0.0001])
+YERR = np.array([0.007, 0.0066, 0.0054, 0.0054, 0.0047, 0.0083, 0.0082, 0.0076, 0.0003])
+
+
+@pytest.mark.parametrize("units", [["mW", None], [None, None]])
+def test_saturation_curve_unitless_no_err(units):
+    """Create saturation curve plot with unitless data and no error bars."""
+    xunit, yunit = units
+    _ = sc.saturation_curve(
+        XDAT, YDAT, xunit=xunit, yunit=yunit, xlabel="Powdiance", fit=False
+    )
+
+
+def test_saturation_curve_unitful_no_err():
+    """Create saturation curve plot with unitful data and no error bars."""
+    xdat = XDAT * ureg.W / ureg.cm**2
+    ydat = YDAT * ureg.counts
+    _ = sc.saturation_curve(xdat, ydat)
+
+
+def test_saturation_curve_yerr():
+    """Plot a saturation curve with error bars on yaxis only."""
+    xdat = XDAT * ureg.W / ureg.cm**2
+    ydat = np.stack([YDAT, YERR])
+    _ = sc.saturation_curve(xdat, ydat)
+
+
+def test_saturation_curve():
+    """Plot saturation curve with errors in both axes."""
+    xdat = np.stack([XDAT, XDAT / 10])
+    ydat = np.stack([YDAT, YERR])
+    _ = sc.saturation_curve(xdat, ydat, xunit="mW", yunit="count")
