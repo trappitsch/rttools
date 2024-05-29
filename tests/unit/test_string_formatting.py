b01d4af6f767190a22e22a953e9d4ce18c7b5d9f
Reto Trappitsch <reto@galactic-forensics.space>
2024-05-29 15:59:23 +0200

    string formatter (#1)
    * add first iteration of string formatter
    
    * add times and \, for space replacements
    
    * handling for None case
    
    * add string formatter to docs

---
+++ tests/unit/test_string_formatting.py
@@ -0,0 +1,37 @@
+"""Test the overarching string formatting functions."""
+
+import pytest
+
+from rttools import StringFmt
+
+
+def test_identity():
+    """Check that the same string goes in as comes out, if the type is the same."""
+    string = r"$^{3}P_{33}$"
+    string_type = StringFmt.Type.latex
+
+    cls = StringFmt(string, string_type)
+    assert cls.latex == string
+
+
+@pytest.mark.parametrize(
+    "cases",
+    [
+        [r"$^{3}P_{33}$", "<sup>3</sup>P<sub>33</sub>"],
+        [r"^{3}P_{33}", "<sup>3</sup>P<sub>33</sub>"],
+        [r"^3P_9", "<sup>3</sup>P<sub>9</sub>"],
+        [r"^3P{\circ}", "<sup>3</sup>P°"],
+        ["^3P{\\circ}", "<sup>3</sup>P°"],
+        [r"5 \times 10^{7}", "5 × 10<sup>7</sup>"],
+        [r"5\,\times 10^{7}", "5 × 10<sup>7</sup>"],
+        [r"$^{3}$F$_{2}$", "<sup>3</sup>F<sub>2</sub>"],
+        [None, None],
+    ],
+)
+def test_html(cases):
+    """Check for correct LaTeX to html conversion."""
+    string_latex = cases[0]
+    string_type = StringFmt.Type.latex
+
+    cls = StringFmt(string_latex, string_type)
+    assert cls.html == cases[1]
